569df4b08aad5dc14aff2f5ad6e542cd41b7744e
Corrado <corradoct@hotmail.it>
2020-06-04 00:48:48 +0200

    Ho completato il gioco.

--- js/function.js
+++ js/function.js
@@ -1,3 +1,52 @@
+// Funzione che crea un array di numeri non inserendo i numeri doppi.
+// Accetta come argomenti il numero minimo e il numero massimo (inclusi) del range.
+// Ritorna un array.
+
+function generaArray (min, max) {
+  var arrayBomb = [];
+  var i = min;
+  while (i <= max) {
+    var numeroRandomBomb = generaNumeroIntero(1, 100);
+    if (!(arrayBomb.includes(numeroRandomBomb))) {
+      arrayBomb.push(numeroRandomBomb);
+      i++;
+    }
+  }
+  return arrayBomb;
+}
+
+// Funzione che genera un numero intero random entro un range.
+// Accetta come argomenti il numero minimo del range e il numero massimo del range,
+// entrambi inclusi.
+// Ritorna il numero generato.
+
 function generaNumeroIntero(min, max) {
   return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+// Funzione che crea un array di numeri scelti dall'utente, controllandoli uno ad uno
+// per vedere se sono gia presenti nell'array delle bombe.
+// Prende come argomento la difficoltà scelta dall'utente e quindi aumenta o diminuisce
+// la probabilità di trovare una bomba.
+// Ritorna false se il numero scelto dall'utente non corrisponde ad una bomba,
+// altrimenti ritorna true e il numero stesso che corrisponde alla bomba.
+
+function userNumbers(difficulty) {
+  var arrayNumeriUtente = [];
+  var controlloNumeroUtente = false;
+  var i = 0;
+  while (controlloNumeroUtente === false && i < difficulty) {
+    var numeroUtente = parseInt(prompt('Hai scelto la difficoltà con ' + difficulty + ' numeri. Inserisci un numero da 1 a 100. Rimangono ' + (difficulty - i) + ' numeri'));
+    while (isNaN(numeroUtente) || numeroUtente > 100 || numeroUtente <= 0 || arrayNumeriUtente.includes(numeroUtente)) {
+      numeroUtente = parseInt(prompt('Errore.Inserisci un numero da 1 a 100'));
+    }
+    if (arrayBombe.includes(numeroUtente)) {
+      controlloNumeroUtente = true;
+      return numeroUtente;
+    } else {
+      arrayNumeriUtente.push(numeroUtente);
+      i++;
+    }
+  }
+  return controlloNumeroUtente;
 }
--- js/script.js
+++ js/script.js
@@ -13,16 +13,35 @@
 
 
 // Creo la variabile dell'elemento
-var lista = document.getElementById('lista');
+var bombe = document.getElementById('bombe');
+var numeriScelti = document.getElementById('numeriScelti');
 
-// Creo array dei 16 numeri casuali da 1 a 100
-var arrayBomb = [];
-var i = 0;
-while (i < 16) {
-  var numeroRandomBomb = generaNumeroIntero(1, 100);
-  if (!(arrayBomb.includes(numeroRandomBomb))) {
-    arrayBomb.push(numeroRandomBomb);
-    i++;
-  }
+// Creo array dei 16 numeri bomba casuali da 1 a 100
+var arrayBombe = generaArray(1, 16);
+
+// Chiedo all'utente di scegliere la difficoltà del gioco
+var difficolta = parseInt(prompt('Scegli il livello di difficoltà : ( 0 => 16 bombe su 100 numeri, 1 => 16 bombe su 80 numeri, 2 => 16 bombe su 50 numeri )'));
+while (isNaN(difficolta) || difficolta >= 3) {
+  difficolta = parseInt(prompt('Errore.Scegli il livello di difficoltà : ( 0 => 16 bombe su 100 numeri, 1 => 16 bombe su 80 numeri, 2 => 16 bombe su 50 numeri )'));
 }
-console.log(arrayBomb);
+switch (difficolta) {
+  case 1:
+    var difficoltaScelta = 80;
+    break;
+  case 2:
+    difficoltaScelta = 50;
+    break;
+  default:
+    difficoltaScelta = 100;
+}
+
+// Chiedo il numero all'utente e controllo se è presente o no tra le bombe
+var resultGame = userNumbers(difficoltaScelta)
+
+// Mostro il risultato all'utente e i numeri bomba
+if (resultGame) {
+  numeriScelti.innerHTML = 'Hai perso, sei esploso con il numero ' + resultGame;
+} else {
+  numeriScelti.innerHTML = 'Hai vinto in ' + difficoltaScelta + ' mosse';
+}
+bombe.innerHTML = arrayBombe;
